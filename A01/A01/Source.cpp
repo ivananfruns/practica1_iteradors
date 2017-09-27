7d552574ef453441866cb396b91116e15778b9e2
Guillem Serra <guillemserralorenz@enti.cat>
2017-09-27 18:31:08 +0200

    vector acabat

--- A01/A01/Source.cpp
+++ A01/A01/Source.cpp
@@ -1,25 +1,70 @@
-// practica1iteradores.cpp : Defines the entry point for the console application.
-//
+
 #include "Header.h"
 
 void main()
 {
-	//iteradores de vector
+	//VECTOR:*******************************************************************************************************************************************
+	std::cout << "*****VECTOR*****\n\n\n";
+
 	std::vector<int> v{ 1,2,3,4,5,6 };
-	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) //tambien se puede reverse con reverse_iterator i r.begin() y rend()
-	{
-		std::cout << *it << std::endl;
-	}
-	for (int i = 0; i < v.size(); i++)
-	{
-		std::cout << v[i] << std::endl;
-	}
-	for (int e : v) {
-		std::cout << e << std::endl;
-	};
+
+	//Forward:
+	std::cout << " Iterador Forward:\n\n";
+	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+		std::cout << *it << " "; 	
+	std::cout << std::endl<<std::endl;
+
+	// Constant Forward
+	std::cout << " Iterador Constant Forward:\n\n";
+	for (std::vector<int>::const_iterator it= v.cbegin(); it != v.end(); ++it)
+		std::cout << *it << " ";
 	std::cout << std::endl;
 
-	//iteradores de deque
+	//Backward:
+	std::cout << " Iterador Backward:\n\n";
+	for (std::vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl << std::endl;
+
+	//Constant Backwards:
+	std::cout << " Iterador Constant Backward:\n\n";
+	for (std::vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl << std::endl;
+
+	//Random Access:
+	std::cout << " Iterador Random Access:\n\n";
+	for (int i = 0; i < v.size(); i++)
+		std::cout << v[i] << " ";
+	std::cout << std::endl << std::endl;
+
+	//Access by value using a copy:
+	std::cout << " Iterador por copia:\n\n";
+	for (int e : v)
+		std::cout << e << " ";
+	std::cout << std::endl << std::endl;
+
+	//Access by constant value using a copy:
+	std::cout << " Iterador por copia con un valor constante:\n\n";
+	for (const int e : v)
+		std::cout << e << " ";
+	std::cout << std::endl << std::endl;
+
+	//Type inference by reference:
+	std::cout << " Iterador por referencia:\n\n";
+	for (int e : v)
+		std::cout << e << " ";
+	std::cout << std::endl << std::endl;
+
+	//Type inference by reference with constant value:
+	std::cout << " Iterador por referencia con valor constante:\n\n";
+	for (int e : v)
+		std::cout << e << " ";
+	std::cout << std::endl << std::endl;
+
+
+	//DEQUE:*******************************************************************************************************************************************
+	std::cout << "*****DEQUE*****\n\n\n";
 	std::deque<int> d{ 1,2,3,4,5,6 };
 	for (std::deque<int>::iterator it = d.begin(); it != d.end(); ++it) //tambien se puede reverse con reverse_iterator i rbegin() y rend()
 	{
